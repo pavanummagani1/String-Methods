5eb32aa9b82a4efbb3326c52e750247f73948eea
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-12-08 23:17:49 +0530

    STRINGS METHODS

--- script.js
+++ script.js
@@ -10,11 +10,87 @@ var str = "Hello Javascript";
 str = str.replaceAll('a', 'A');
 console.log(str); //OUTPUT: Hello JAvAscript;
 
-//Charat()
+//3.Charat()
 var str = "Hello Javascript";
 str = str.charAt(45);
 console.log(str);
 
-//at()
+//4. at()
 var str = "Hello Javascript";
 console.log(str.at(33));
+
+//5. concat()
+var str = "Hello Javascript";
+console.log(str);
+
+//6. indexOf()
+var str = "Hello Javascript";
+console.log(str);
+
+//7. lastIndexOf()
+var str = "Hello Javascript";
+console.log(str);
+
+//8. length()
+var str = "Hello Javascript";
+console.log(str);
+
+//9 .padStrat
+var str = "Hello Javascript";
+console.log(str);
+
+//10. padEnd
+var str = "Hello Javascript";
+console.log(str);
+
+//11. toLowerCase()
+var str = "Hello Javascript";
+console.log(str);
+
+//12. toUpperCase()
+var str = "Hello Javascript";
+console.log(str);
+
+//13. startsWith()
+var str = "Hello Javascript";
+console.log(str);
+
+//14. endsWith()
+var str = "Hello Javascript";
+console.log(str);
+
+//15. trim()
+var str = "Hello Javascript";
+console.log(str);
+
+//16 trimRight()
+var str = "Hello Javascript";
+console.log(str);
+
+//17 trimLeft()
+var str = "Hello Javascript";
+console.log(str);
+
+//18 trimStart()
+var str = "Hello Javascript";
+console.log(str);
+
+//19 trimEnd()
+var str = "Hello Javascript";
+console.log(str);
+
+//20 subString
+var str = "Hello Javascript";
+console.log(str);
+
+//21 substr()
+var str = "Hello Javascript";
+console.log(str);
+
+//22 split()
+var str = "Hello Javascript";
+console.log(str);
+
+//25 slice();
+var str = "Hello Javascript";
+console.log(str);
